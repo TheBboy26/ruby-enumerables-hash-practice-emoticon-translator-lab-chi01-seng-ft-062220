219234648bfac2b3f58e005262614c2ec804787f
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-19 15:35:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,18 @@
 require "yaml"
 
 def load_library
-emoticon = YAML
+emoticon = YAML.load_file(file_path)
+new_hash ={}
+
+emoticon.each do |key,value|
+  new_hash[key] = {}
+  new_hash[key][:english] = value[0]
+  new_hash[key]][:japanese] = value[1]
 end
+new_hash
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
 def get_english_meaning
