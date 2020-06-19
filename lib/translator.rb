889bcfc453d41889dda937d7c5953fea46cbd706
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-19 15:37:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,17 @@
+  
 require "yaml"
 
 def load_library(file_path)
-emoticon = YAML.load_file(file_path)
-new_hash = {}
+  emoticons = YAML.load_file(file_path)
+  new_hash = {}
 
-emoticon.each do |key,value|
-  new_hash[key] = {}
-  new_hash[key][:english] = value[0]
-  new_hash[key]][:japanese] = value[1]
-end
-new_hash
+  emoticons.each do |key,value|
+    new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
+  end
+
+  new_hash
 end
 
 def get_japanese_emoticon(file_path, emoticon)
